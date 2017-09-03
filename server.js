300d27350b242b8599946769c2589291d9e8a3d6
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-03 12:34:21 -0400

    adds route for Recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,18 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('Cookies', ['flour', 'milk']);
+Recipes.create('Muffin', ['flour', 'milk', 'butter']);
+Recipes.create('Pudding', ['flour', 'milk', 'butter', 'gelatin']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
